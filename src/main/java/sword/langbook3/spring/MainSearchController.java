34d998d808c983e0dab9f056dc948859576f26ab
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-07-14 17:49:57 +0200

    Display the contents of a list in the HTML

--- src/main/java/sword/langbook3/spring/MainSearchController.java
+++ src/main/java/sword/langbook3/spring/MainSearchController.java
@@ -1,12 +1,23 @@
 package sword.langbook3.spring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import sword.collections.ImmutableList;
 
 @Controller
 public final class MainSearchController {
     @GetMapping("/")
-    public String mainSearch() {
+    public String mainSearch(@RequestParam(name="query", required=false) String query, Model model) {
+        final ImmutableList<String> texts = new ImmutableList.Builder<String>()
+                .append("a")
+                .append("b")
+                .append("c")
+                .build();
+
+        model.addAttribute("query", query);
+        model.addAttribute("texts", texts);
         return "main_search";
     }
 }
