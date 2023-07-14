03f033375dba4f84590ef91166db97325589a745
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-07-14 17:57:53 +0200

    Include second step for addLanguage intention

--- src/main/java/sword/langbook3/spring/AddLanguageIntentionController.java
+++ src/main/java/sword/langbook3/spring/AddLanguageIntentionController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sword.collections.ImmutableIntRange;
 import sword.collections.ImmutableList;
 import sword.collections.ImmutableMap;
 import sword.langbook3.spring.db.AlphabetId;
 import sword.langbook3.spring.db.LangbookDbManagerImpl;
 import sword.langbook3.spring.db.LanguageId;
+
+import java.util.Map;
 
 @Controller
 public final class AddLanguageIntentionController {
@@ -35,14 +38,28 @@ public final class AddLanguageIntentionController {
     }
 
     @GetMapping("/intention/addLanguage")
-    public String addLanguage(@RequestParam(name = "languageCode", required = false) String languageCode, @RequestParam(name = "alphabetCount", required = false) String alphabetCount, Model model) {
-        if (isValidLanguageCode(languageCode) && isValidAlphabetCount(alphabetCount)) {
-            // TODO: This should point to the second step of this intention
-            return "main_search";
+    public String addLanguage(@RequestParam Map<String, String> requestParams, Model model) {
+        final String languageCode = requestParams.get("languageCode");
+        final String alphabetCountStr = requestParams.get("alphabetCount");
+        model.addAttribute("languageCode", languageCode);
+        model.addAttribute("alphabetCount", alphabetCountStr);
+
+        if (isValidLanguageCode(languageCode) && isValidAlphabetCount(alphabetCountStr)) {
+            final int alphabetCount = Integer.parseInt(alphabetCountStr);
+            final ImmutableList<String> alphabetIds = new ImmutableIntRange(1, alphabetCount).map(i -> "a" + i);
+
+            final ImmutableList<String> languageTexts = alphabetIds.map(id -> requestParams.get("l" + id));
+            if (languageTexts.allMatch(t -> t != null && t.length() > 0)) {
+                // TODO: We should jump to the correlation picker for the language
+                return "main_search";
+            }
+            else {
+                model.addAttribute("languageId", "l");
+                model.addAttribute("alphabetIds", alphabetIds);
+                return "add_language_language_edition";
+            }
         }
         else {
-            model.addAttribute("languageCode", languageCode);
-            model.addAttribute("alphabetCount", alphabetCount);
             return "add_language";
         }
     }
