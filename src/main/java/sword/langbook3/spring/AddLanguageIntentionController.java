fefe753ea29d3b439b7028ac14c31a42081eeaac
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-07-14 17:58:51 +0200

    Include correlation picker for language in addLanguage intention

--- src/main/java/sword/langbook3/spring/AddLanguageIntentionController.java
+++ src/main/java/sword/langbook3/spring/AddLanguageIntentionController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import sword.collections.ImmutableIntRange;
-import sword.collections.ImmutableList;
-import sword.collections.ImmutableMap;
+import sword.collections.*;
+import sword.langbook3.android.db.ImmutableCorrelation;
+import sword.langbook3.android.db.ImmutableCorrelationArray;
 import sword.langbook3.spring.db.AlphabetId;
 import sword.langbook3.spring.db.LangbookDbManagerImpl;
 import sword.langbook3.spring.db.LanguageId;
@@ -37,6 +37,27 @@ public final class AddLanguageIntentionController {
         }
     }
 
+    public record CorrelationArrayOption(String id, String text) {
+    }
+
+    private static final char COMPOSED_ID_SEPARATOR = '.';
+
+    private String composeId(ImmutableCorrelationArray<String> option) {
+        // Currently this will fail if texts include separator
+        // TODO: Changing this logic to allow the separator characters
+        return option
+                .map(corr -> corr.sort(SortUtils::compareCharSequenceByUnicode).reduce((a, b) -> a + COMPOSED_ID_SEPARATOR + b))
+                .reduce((a, b) -> a + COMPOSED_ID_SEPARATOR + b);
+    }
+
+    private String composeText(ImmutableCorrelationArray<String> option) {
+        // Currently this will fail if texts include ','
+        // TODO: Changing this logic to allow the separator characters
+        return option
+                .map(corr -> corr.sort(SortUtils::compareCharSequenceByUnicode).reduce((a, b) -> a + '/' + b))
+                .reduce((a, b) -> a + " + " + b);
+    }
+
     @GetMapping("/intention/addLanguage")
     public String addLanguage(@RequestParam Map<String, String> requestParams, Model model) {
         final String languageCode = requestParams.get("languageCode");
@@ -50,8 +71,16 @@ public final class AddLanguageIntentionController {
 
             final ImmutableList<String> languageTexts = alphabetIds.map(id -> requestParams.get("l" + id));
             if (languageTexts.allMatch(t -> t != null && t.length() > 0)) {
-                // TODO: We should jump to the correlation picker for the language
-                return "main_search";
+                ImmutableCorrelation<String> correlation = ImmutableCorrelation.empty();
+                for (String alphabetId : alphabetIds) {
+                    correlation = correlation.put(alphabetId, requestParams.get("l" + alphabetId));
+                }
+
+                final ImmutableList<CorrelationArrayOption> options = correlation
+                        .checkPossibleCorrelationArrays(SortUtils::compareCharSequenceByUnicode)
+                        .map(opt -> new CorrelationArrayOption(composeId(opt), composeText(opt)));
+                model.addAttribute("correlations", options);
+                return "add_language_language_correlation_picker";
             }
             else {
                 model.addAttribute("languageId", "l");
