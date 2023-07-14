f014ffdf1dd3f1993e8924d5dca3c78df09bf547
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2023-07-14 17:45:32 +0200

    Initial commit with basic spring application

---
+++ src/main/java/sword/langbook3/spring/LangbookApplication.java
@@ -0,0 +1,13 @@
+package sword.langbook3.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sword/langbook3/spring/LangbookApplicationTests.java
@@ -0,0 +1,13 @@
+package sword.langbook3.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
